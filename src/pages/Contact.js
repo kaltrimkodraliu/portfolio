666d65618339be1f77ed140cb080994f5c4be2ba
Kaltrim Kodraliu <kodraliukaltrim@gmail.com>
2022-11-04 19:33:57 +0100

    Arrangement of pages and components

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import HeroImg2 from "./components/HeroImg2";
-import Form from "./components/Form";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
+import Form from "../components/Form";
 
 
 const Contact = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
-import HeroImg from "./components/HeroImg";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Work from "./components/Work";
+import HeroImg from "../components/HeroImg";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Work from "../components/Work";
 
 const Home = () => {
     return <div>
