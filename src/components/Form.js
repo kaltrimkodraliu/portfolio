211f48a85b08267c0e1689ac329a315c3e76bafe
Kaltrim Kodraliu <kodraliukaltrim@gmail.com>
2022-11-08 03:49:30 +0100

    Implemented Contact Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,15 @@ import React from "react"
 const Form = () => {
     return (
         <div className="form">
-            <form>
+            <form action="https://formspree.io/f/xvoydgqj"
+                method="POST">
                 <label>Your Name</label>
                 <input type="text"></input>
                 <label>Email</label>
-                <input type="email"></input>
-                <label>Subject</label>
-                <input type="text"></input>
+                <input type="email" name="email"></input>
                 <label>Message</label>
-                <textarea rows="6" placeholder="Type your message here" />
-                <button className="btn">Submit</button>
+                <textarea rows="6" placeholder="Type your message here" name="message"></textarea>
+                <button className="btn" type="submit">Submit</button>
             </form>
         </div>
     )
