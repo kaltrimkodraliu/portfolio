c484c6d73ffcc9b6df0daf6d1cf8bf8d05642b67
Kaltrim Kodraliu <kodraliukaltrim@gmail.com>
2022-11-12 04:47:18 +0100

    Last changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,9 +7,9 @@ const Form = () => {
             <form action="https://formspree.io/f/xvoydgqj"
                 method="POST">
                 <label>Your Name</label>
-                <input type="text" required ></input>
+                <input type="text" required placeholder="Enter your name"></input>
                 <label>Email</label>
-                <input type="email" required name="email"></input>
+                <input type="email" required name="email" placeholder="Enter your email"></input>
                 <label>Message</label>
                 <textarea rows="6" placeholder="Type your message here" name="message"></textarea>
                 <button className="btn" type="submit">Submit</button>
