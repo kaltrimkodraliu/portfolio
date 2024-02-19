1b815fea16368c85363111a23807455e9c629604
kaltrimkodraliu <kodraliukaltrim@gmail.com>
2024-02-19 16:52:47 +0100

    fix: change cv content

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,24 +2,36 @@ import "../styles/AboutContentStyle.css";
 import React from "react";
 
 const AboutContent = () => {
-    return (
-        <div className="about">
-            <div className="left">
-                <h1>Who Am I?</h1>
-                <p>Im a react front-end developer. I create
-                    responsive secure websites for my clients.
-                </p>
-                <a className='btn mt-3' href={process.env.PUBLIC_URL + "/assets/documents/Kaltrim Kodraliu - CV.pdf"} download>Download CV</a>
-            </div>
-            <div className="right">
-                <div className="img-container">
-                    <div className="img-stack top">
-                        <h1>Skills</h1>
-                        <img src={process.env.PUBLIC_URL + "/assets/1.png"} alt="true"></img>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>Who Am I?</h1>
+        <p>
+          Im a react front-end developer. I create responsive secure websites
+          for my clients.
+        </p>
+        <a
+          className="btn mt-3"
+          href={
+            process.env.PUBLIC_URL + "/assets/documents/Kaltrim Kodraliu CV.pdf"
+          }
+          download
+        >
+          Download CV
+        </a>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <h1>Skills</h1>
+            <img
+              src={process.env.PUBLIC_URL + "/assets/1.png"}
+              alt="true"
+            ></img>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 export default AboutContent;
